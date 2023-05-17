cf032bcfa615149a9fb888da3cf5c638a6a0786a
project-dy <kero122899@gmail.com>
2023-05-17 15:35:06 +0900

    init

--- ch11/11.1/nodebird/node-test.mjs
+++
@@ -1,6 +0,0 @@
-import test from 'node:test';
-import assert from 'assert';
-
-test('1 + 1은 2입니다.', () => {
-  assert.strictEqual(1 + 1, 2);
-});
--- ch17/17.4/nodebird/models/__mocks__/index.ts
+++
@@ -1,3 +0,0 @@
-export const sequelize = {
-  sync: () => Promise.resolve(),
-};
